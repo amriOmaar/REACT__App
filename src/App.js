a59e2205e6d93b888b3f6cd6b1e2354d9ff7ec82
Mahdi El Ghali <60888933+mahdielghali@users.noreply.github.com>
2023-02-16 17:19:21 +0100

    Functional Component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import FunctUseState from "./Components/FunctUseState";
+import Products from "./Components/Products"
+import Products1 from "./Components/Products1";
+function App() {
+  return (
+    <>
+      <Products1></Products1>
+      {/* <FunctUseState></FunctUseState> */}
+    </>
+  );
+}
+
+export default App;
+
+
+
+
