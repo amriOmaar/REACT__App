5b08b58a56b6760d6827d94c176442d3551e347c
Omar AMRI <amri.omar93@gmail.com>
2023-02-23 14:31:29 +0100

    Hello Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import FunctUseState from "./Components/FunctUseState";
-import Products from "./Components/Products"
-import Products1 from "./Components/Products1";
+import Hello from "./Components/Hello";
+// import FunctUseState from "./Components/FunctUseState";
+// import Products from "./Components/Products"
+// import Products1 from "./Components/Products1";
+import Home from "./Components/Home";
+import NavBar from "./Components/NavBar";
 function App() {
   return (
     <>
-      <Products1></Products1>
+      {/* <Products1></Products1> */}
       {/* <FunctUseState></FunctUseState> */}
+      <NavBar />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path= "/Hello" element={ <Hello /> } />
+      </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import MyContext from "./Components/MyContext";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 root.render(
   // <React.StrictMode>
-    <MyContext.Provider value={{username:"sarra", loggedIn:true}} >
+    <MyContext.Provider value={{username:"omaar", loggedIn:true}} >
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
     </MyContext.Provider>
   // </React.StrictMode>
 );
